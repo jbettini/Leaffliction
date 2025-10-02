24da954f5ffe85e45c7e32678e90c56cc36bacd4
jbettini <ashhxtm@gmail.com>
2025-10-03 01:27:51 +0200

    Fix deadlink

--- P1/prepare_images.sh
+++ P1/prepare_images.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -sOf https://cdn.intra.42.fr/document/document/17447/leaves.zip
+set -eu
+
+curl -sOf https://cdn.intra.42.fr/document/document/39824/leaves.zip
 unzip leaves.zip
 pushd images
     mkdir -vp Apples Grapes
