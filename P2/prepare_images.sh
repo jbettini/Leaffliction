a846145c0d6a08c52f047790f34b49e4d01224c5
jbettini <ashhxtm@gmail.com>
2025-11-13 19:55:11 -0500

    Edit prepare.sh for P2

--- P2/prepare_images.sh
+++ P2/prepare_images.sh
@@ -4,9 +4,4 @@ set -eu
 
 curl -sOf https://cdn.intra.42.fr/document/document/39824/leaves.zip
 unzip leaves.zip
-pushd images
-    mkdir -vp Apples Grapes
-    mv Apple_* Apples/
-    mv Grape_* Grapes/
-popd
 rm leaves.zip
